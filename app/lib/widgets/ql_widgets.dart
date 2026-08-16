eaeea9b170c577573819d24253eae05413d58190
mrrewilh <rewilhgameacc@hotmail.com>
2026-08-16 21:00:28 +0300

    center text in fields

--- app/lib/widgets/ql_widgets.dart
+++ app/lib/widgets/ql_widgets.dart
@@ -172,6 +172,7 @@ class _QLTextFieldState extends State<QLTextField> {
           controller: widget.controller,
           autofocus: widget.autofocus,
           onChanged: widget.onChanged,
+          textAlignVertical: TextAlignVertical.center,
           style: TextStyle(color: c.textPrimary, fontSize: 13),
           decoration: InputDecoration(
             hintText: widget.hint,
@@ -186,7 +187,6 @@ class _QLTextFieldState extends State<QLTextField> {
               horizontal: 14,
               vertical: 10,
             ),
-            isDense: true,
           ),
         ),
       ),
